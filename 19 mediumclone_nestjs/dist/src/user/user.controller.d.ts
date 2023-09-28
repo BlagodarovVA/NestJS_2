d202851dbfe29039b868fa8f9579ea375dd9162b
Valery Blagodarov <11w-walerik@mail.ru>
2023-09-28 11:52:49 +0300

    19 done

---
+++ 19 mediumclone_nestjs/dist/src/user/user.controller.d.ts
@@ -0,0 +1,8 @@
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/createUser.dto';
+import { UserEntity } from './user.entity';
+export declare class UserController {
+    private readonly userService;
+    constructor(userService: UserService);
+    createUser(createUserDto: CreateUserDto): Promise<UserEntity>;
+}
