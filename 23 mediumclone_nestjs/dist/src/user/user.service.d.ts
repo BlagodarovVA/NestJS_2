3c072d02f718dffdbb413db235b4f4c19d67d61b
Valery Blagodarov <11w-walerik@mail.ru>
2023-10-02 16:07:36 +0300

    23 done

---
+++ 23 mediumclone_nestjs/dist/src/user/user.service.d.ts
@@ -0,0 +1,10 @@
+import { CreateUserDto } from './dto/createUser.dto';
+import { UserEntity } from './user.entity';
+import { Repository } from 'typeorm';
+export declare class UserService {
+    private readonly userRepository;
+    constructor(userRepository: Repository<UserEntity>);
+    createUser(createUserDto: CreateUserDto): Promise<UserEntity>;
+    generateJwt(user: UserEntity): string;
+    buildUserResponse(user: UserEntity): any;
+}
