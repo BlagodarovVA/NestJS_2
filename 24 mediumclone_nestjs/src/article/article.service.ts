2dc3d877dd1e33723547fa5f89d55a7cc1deb572
Valery Blagodarov <11w-walerik@mail.ru>
2023-10-03 17:52:51 +0300

    24 done

---
+++ 24 mediumclone_nestjs/src/article/article.service.ts
@@ -0,0 +1,31 @@
+import { UserEntity } from '@app/user/user.entity';
+import { Injectable } from '@nestjs/common';
+import { CreateArticleDto } from './dto/createArticle.dto';
+import { ArticleEntity } from './article.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class ArticleService {
+  constructor(
+    @InjectRepository(ArticleEntity)
+    private readonly articleRepository: Repository<ArticleEntity>,
+  ) {}
+  async createArticle(
+    currentUser: UserEntity,
+    createArticleDto: CreateArticleDto,
+  ): Promise<ArticleEntity> {
+    const article = new ArticleEntity();
+    Object.assign(article, createArticleDto);
+
+    if (!article.tagList) {
+      article.tagList = [];
+    }
+
+    article.slug = 'zaglushka';
+
+    article.author = currentUser;
+
+    return await this.articleRepository.save(article);
+  }
+}
