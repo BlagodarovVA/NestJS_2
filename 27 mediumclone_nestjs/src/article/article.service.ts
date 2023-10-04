be4889480f0206064391cdcd478f9825fc4fa16f
Valery Blagodarov <11w-walerik@mail.ru>
2023-10-04 17:17:12 +0300

    27 done

---
+++ 27 mediumclone_nestjs/src/article/article.service.ts
@@ -0,0 +1,65 @@
+import { UserEntity } from '@app/user/user.entity';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { CreateArticleDto } from './dto/createArticle.dto';
+import { ArticleEntity } from './article.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DeleteResult, Repository } from 'typeorm';
+import { ArticleResponseInterface } from './types/articleResponse.interface';
+import slugify from 'slugify';
+
+@Injectable()
+export class ArticleService {
+  constructor(
+    @InjectRepository(ArticleEntity)
+    private readonly articleRepository: Repository<ArticleEntity>,
+  ) {}
+  async createArticle(
+    currentUser: UserEntity,
+    createArticleDto: CreateArticleDto,
+  ): Promise<ArticleEntity> {
+    const article = new ArticleEntity();
+    Object.assign(article, createArticleDto);
+
+    if (!article.tagList) {
+      article.tagList = [];
+    }
+
+    article.slug = this.getSlug(createArticleDto.title);
+    article.author = currentUser;
+
+    return await this.articleRepository.save(article);
+  }
+
+  async findBySlug(slug: string): Promise<ArticleEntity> {
+    return await this.articleRepository.findOne({ where: { slug } });
+  }
+
+  async deleteArticle(
+    slug: string,
+    currentUserId: number,
+  ): Promise<DeleteResult> {
+    const article = await this.findBySlug(slug);
+
+    if (!article) {
+      throw new HttpException('Article does not exist', HttpStatus.NOT_FOUND);
+    }
+
+    if (article.author.id !== currentUserId) {
+      throw new HttpException('You are not the autor', HttpStatus.FORBIDDEN);
+    }
+
+    return await this.articleRepository.delete({ slug });
+  }
+
+  buildArticleResponse(article: ArticleEntity): ArticleResponseInterface {
+    return { article };
+  }
+
+  private getSlug(title: string): string {
+    return (
+      slugify(title, { lower: true }) +
+      '-' +
+      ((Math.random() * Math.pow(36, 6)) | 0).toString(36)
+    );
+  }
+}
