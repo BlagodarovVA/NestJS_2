5703730c4c5fd01b196c8536ca906eb696a01dc2
Valery Blagodarov <11w-walerik@mail.ru>
2023-10-19 17:03:29 +0300

    30 done

---
+++ 30 mediumclone_nestjs/dist/src/main.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+if (!process.env.IS_TS_NODE) {
+    require('module-alias/register');
+}
+const core_1 = require("@nestjs/core");
+const app_module_1 = require("./app.module");
+async function bootstrap() {
+    const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    await app.listen(3000);
+}
+bootstrap();
+console.log('process.env.IS_TS_NODE (true - не прод!!!):', process.env.IS_TS_NODE);
+//# sourceMappingURL=main.js.map
